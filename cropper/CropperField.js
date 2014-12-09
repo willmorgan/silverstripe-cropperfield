038ab3bf448a2b66cf1cd81dd88c19246dd561b9
Will Morgan <me@willmorgan.co.uk>
2014-12-09 10:47:22 +0000

    JS: re-enable on page save & pjax

--- cropper/CropperField.js
+++ cropper/CropperField.js
@@ -1,16 +1,32 @@
 (function($) {
-	$('.js-cropperfield').each(function() {
-		var field = $(this);
-		var image = field.find('.js-cropperfield__target');
-		var dataField = field.find('.js-cropperfield__data');
-		var fieldID = field.attr('data-field-id');
-		image.cropper({
-			multiple: true,
-			autoCropArea: 1,
-			done: function(data) {
-				dataField.val(JSON.stringify(data));
-			}
-		});
 
-	});
+	var CropperField = {
+		init: function() {
+			var field = $(this);
+			var image = field.find('.js-cropperfield__target');
+			var dataField = field.find('.js-cropperfield__data');
+			var fieldID = field.attr('data-field-id');
+			image.cropper({
+				multiple: true,
+				autoCropArea: .8,
+				done: function(data) {
+					dataField.val(JSON.stringify(data));
+				}
+			});
+		}
+	};
+
+	$('.js-cropperfield').each(CropperField.init);
+
+	// Set the init method to re-run if the page is saved or pjaxed
+	if($.entwine) {
+		$.entwine('ss', function($) {
+			$('.js-cropperfield').entwine({
+				onmatch: function() {
+					$('.js-cropperfield').each(CropperField.init);
+				}
+			});
+		});
+	}
+
 }(jQuery));
