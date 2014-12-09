497a3f899fb999e38d854350c2759fdc6ad9050d
Will Morgan <me@willmorgan.co.uk>
2014-12-09 15:39:04 +0000

    Basic frontend amendments
    - Add a label as to the cropping mode
    - Try to write some proper JS for once :)

--- cropper/CropperField.js
+++ cropper/CropperField.js
@@ -1,46 +1,108 @@
 (function($) {
 
-	var CropperField = {
-		init: function() {
-			var field = $(this);
-			var options = JSCONFIG.CropperField[field.attr('data-field-id')];
-			var image = field.find('.js-cropperfield__target');
-			var dataField = field.find('.js-cropperfield__data');
-			var toggleField = field.find('.js-cropperfield__toggle');
+	var CropperField = function(field) {
+		var _inst = this;
+		var target = field.find('.js-cropperfield__target');
+		var dataField = field.find('.js-cropperfield__data');
+		var cropperHeight;
+		var construct = function() {
+			var options = JSCONFIG.CropperField[_inst.id()];
 			var fieldID = field.attr('data-field-id');
 			var preview = field.find('.cropperfield__preview');
-			var onToggle = function() {
-				var method = $(this).is(':checked') ? 'enable' : 'disable';
-				var inClass = method == 'enable' ? 'cropperfield--cropping' : 'cropperfield--inactive';
-				var outClass = method !== 'enable' ? 'cropperfield--cropping' : 'cropperfield--inactive';
-				image.cropper(method);
-				field.removeClass(outClass).addClass(inClass);
-			};
-			image.cropper({
+			target.cropper({
 				multiple: true,
 				aspectRatio: options.aspect_ratio,
 				autoCropArea: .8,
 				preview: preview,
 				done: function(data) {
 					dataField.val(JSON.stringify(data));
+				},
+				built: function() {
+					_inst.setCropperHeight(
+						field.find('.cropper-container')[0].clientHeight
+					);
 				}
 			});
-			toggleField.on('change click', onToggle);
-			onToggle.call(toggleField);
+			target.cropper('disable');
+			setupCheckbox();
+			CropperField.instances.push(_inst);
+			return this;
+		};
+		var setupCheckbox = function() {
+			var toggleField = field.find('.js-cropperfield__toggle');
+			toggleField.on('change click', onCheckboxToggle);
+			onCheckboxToggle.call(toggleField);
+		};
+		var onCheckboxToggle = function() {
+			var method = $(this).is(':checked') ? 'enable' : 'disable';
+			target.cropper(method);
+			_inst[method]();
+		};
+		this.id = function() {
+			return field.attr('data-field-id');
+		};
+		this.setCropperHeight = function(height) {
+			cropperHeight = height;
+			return this;
 		}
+		this.getCropperHeight = function() {
+			return cropperHeight || 600;
+		};
+		this.enable = function() {
+			var inClass = 'cropperfield--cropping';
+			var outClass = 'cropperfield--inactive';
+			field.removeClass(outClass).addClass(inClass);
+			field.find('.cropperfield__cropper-container').attr(
+				'style',
+				'height: ' + _inst.getCropperHeight() + 'px'
+			);
+			return this;
+		};
+		this.disable = function() {
+			var inClass = 'cropperfield--inactive';
+			var outClass = 'cropperfield--cropping';
+			field.removeClass(outClass).addClass(inClass);
+			field.find('.cropperfield__cropper-container').removeAttr('style');
+			return this;
+		};
+		this.destroy = function() {
+			_inst.disable();
+			target.cropper('destroy');
+			return this;
+		};
+		construct();
+		return this;
 	};
 
-	$('.js-cropperfield').each(CropperField.init);
+	CropperField.instances = [];
 
-	// Set the init method to re-run if the page is saved or pjaxed
-	if($.entwine) {
-		$.entwine('ss', function($) {
-			$('.js-cropperfield').entwine({
-				onmatch: function() {
-					$('.js-cropperfield').each(CropperField.init);
-				}
-			});
+	CropperField.destroy = function() {
+		CropperField.instances.forEach(function(instance, key, collection) {
+			instance.destroy();
+			delete collection[key];
 		});
 	}
+
+	$(function() {
+
+		$('.js-cropperfield').each(function() {
+			new CropperField($(this));
+		});
+
+		// Set the init method to re-run if the page is saved or pjaxed
+		if($.entwine) {
+			$.entwine('ss', function($) {
+				$('.js-cropperfield').entwine({
+					onmatch: function() {
+						CropperField.destroy();
+						$('.js-cropperfield').each(function() {
+							new CropperField($(this));
+						});
+					}
+				});
+			});
+		}
+
+	});
 
 }(jQuery));
--- templates/CropperField.ss
+++ templates/CropperField.ss
@@ -2,21 +2,29 @@
 	<div class="cropperfield__thumbarea">
 		<div class="cropperfield__existing">
 			$ExistingThumbnail
+			<span class="cropperfield__label cropperfield__label--existing">
+				<%t CropperField.LABELS.Existing "Existing Crop" %>
+			</span>
 		</div>
 		<div class="cropperfield__preview-container">
 			<div class="cropperfield__preview"></div>
+			<span class="cropperfield__label  cropperfield__label--preview">
+				<%t CropperField.LABELS.Preview "Cropping Preview" %>
+			</span>
+		</div>
+		<div class="cropperfield__controls">
+			<label>
+				<input type="checkbox" class="js-cropperfield__toggle" name="{$Name}[Enabled]" />
+				<%t CropperField.CONTROLS.ToggleEdit "Edit this cropped image" %>
+			</label>
 		</div>
 	</div>
-	<div class="cropperfield__controls">
-		<label>
-			<input type="checkbox" class="js-cropperfield__toggle" name="{$Name}[Enabled]" />
-			<%t CropperField.CONTROLS.Toggle "Recreate thumbnail" %>
-		</label>
-	</div>
-	<div class="cropperfield__cropper">
-		<% with $Adapter %>
-			<img src="$ImageFilename" class="js-cropperfield__target" />
-		<% end_with %>
+	<div class="cropperfield__cropper-container">
+		<div class="cropperfield__cropper">
+			<% with $Adapter %>
+				<img src="$ImageFilename" class="js-cropperfield__target" />
+			<% end_with %>
+		</div>
 	</div>
 	<input type="hidden" class="js-cropperfield__data" name="{$Name}[Data]" />
 </div>
